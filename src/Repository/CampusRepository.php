fa749df49deea0b3d3f710ff0cdd4981abf45cb0
tatiana <pocquet-tatiana@hotmail.fr>
2021-08-23 15:06:08 +0200

    create campus

--- src/Repository/CampusRepository.php
+++ src/Repository/CampusRepository.php
@@ -16,7 +16,6 @@ class CampusRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-
         parent::__construct($registry, Campus::class);
     }
 
