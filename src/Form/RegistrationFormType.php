8b016576da4c6895731fa071ebdeed4d530d8168
tatiana <pocquet-tatiana@hotmail.fr>
2021-08-24 07:49:09 +0200

    maj entity  RegistrationFormType.php + css

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -19,7 +19,7 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('email')
             ->add('username')
-            ->add('plainPassword', PasswordType::class, [
+            ->add('Password', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
