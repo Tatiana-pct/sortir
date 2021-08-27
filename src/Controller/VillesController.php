1f079a37c76559d9670416bf7790a422fe3c9538
tatiana <pocquet-tatiana@hotmail.fr>
2021-08-27 16:16:50 +0200

    mise en place du formulaire d'ajout d'une ville

--- src/Controller/VillesController.php
+++ src/Controller/VillesController.php
@@ -2,12 +2,11 @@
 
 namespace App\Controller;
 
-
-
-
+use App\Entity\Ville;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\VillesType;
 use App\Repository\VilleRepository;
 use Doctrine\ORM\EntityManagerInterface;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 /**
@@ -18,12 +17,25 @@ class VillesController extends AbstractController
     /**
      * @Route("/liste", name="liste")
      */
-    public function afficherVille(VilleRepository $villeRepository)
+    public function afficherVille(VilleRepository $villeRepository, Request $request, EntityManagerInterface $entityManager)
     {
         $ville =$villeRepository->findAll();
+        $Ville = new Ville();
+        $VilleForm =$this->createForm(VillesType::class,$Ville);
+        $VilleForm->handleRequest($request);
+
+        if($VilleForm->isSubmitted() && $VilleForm->isValid()) {
+            $entityManager->persist($Ville);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('villes_liste');
+        }
+
+
 
         return $this->render('villes/list.html.twig',[
-            "ville" => $ville
+            "ville" => $ville,
+            'villeForm' => $VilleForm->createView()
         ]);
 
     }
@@ -34,7 +46,11 @@ class VillesController extends AbstractController
      */
     public function createVille()
     {
-        return $this->render('villes/create.html.twig');
+
+        return $this->render('villes/list.html.twig');
+
+
+
     }
 
 
@@ -53,11 +69,11 @@ class VillesController extends AbstractController
     /**
      * @Route("/delete", name="delete")
      */
-    public function DeleteVille(int $id, EntityManagerInterface $entityManager)
+    public function deleteVille(int $id, EntityManagerInterface $entityManager)
     {
         $ville= new ville();
 
-        $entityManager->remove($villes);
+        $entityManager->remove($ville);
         $entityManager->flush();
         return $this->render('villes/create.html.twig');
     }
---
+++ src/Form/VillesType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ville;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VillesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+           $builder
+               ->add('nom', null,[
+                   'label' => 'Ville',
+                   'required' => true,
+                   'attr' =>[
+                       'class' => 'form-control']
+               ])
+               ->add('codePostal', null,[
+                   'label' => 'Code Postal',
+                   'required' => true,
+                   'attr' =>[
+                       'class' => 'form-control']
+               ]);
+    }
+
+
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ville::class,
+        ]);
+    }
+}
