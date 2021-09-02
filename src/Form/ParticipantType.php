ff88f902c88e5d71582e276c4b4fff6535addc1b
Nicolas Reymond <nicolas.reymond2021@campus-eni.fr>
2021-09-02 11:08:04 +0200

    maj label

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -6,6 +6,7 @@ use App\Entity\Campus;
 use App\Entity\Participant;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,10 +16,10 @@ class ParticipantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('prenom', null, ['label' => 'Prénom'])
-            ->add('nom', null, ['label' => 'Nom'])
-            ->add('telephone', null, ['label' => 'Téléphone'])
+            ->add('email', EmailType::class, ['label' => 'Email :'])
+            ->add('prenom', null, ['label' => 'Prénom :'])
+            ->add('nom', null, ['label' => 'Nom :'])
+            ->add('telephone', null, ['label' => 'Téléphone :'])
             ->add('campus', EntityType::class, [
                 'label' => 'Votre Campus',
                 'class' => Campus::class,
